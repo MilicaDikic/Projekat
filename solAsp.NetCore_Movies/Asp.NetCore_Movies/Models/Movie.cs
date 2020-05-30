1b163dad039c0ed1f9924f6806c08ce7ef81b18d
Danijela Bulovic <dbulovic17@raf.rs>
2020-05-30 13:43:30 +0100

    Dodat je model Movie

---
+++ solAsp.NetCore_Movies/Asp.NetCore_Movies/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Asp.NetCore_Movies.Models
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Ime")]
+        [Required]
+        public string Ime { get; set; }
+
+        [BsonElement("Zanr")]
+        [Required]
+        public string Zanr { get; set; }
+
+
+
+        [BsonElement("Info")]
+        [Required]
+        public string Info { get; set; }
+    }
+}
