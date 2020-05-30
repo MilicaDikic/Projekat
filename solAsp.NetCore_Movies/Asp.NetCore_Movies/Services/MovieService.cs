e1ae88c4f3b0789ea7591e4ac7d0055d92aae6aa
Yazid Shihadeh <yazid998@gmail.com>
2020-05-30 13:59:22 +0200

    Komunikacija sa bazom

---
+++ solAsp.NetCore_Movies/Asp.NetCore_Movies/Services/MovieService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp.NetCore_Movies.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Asp.NetCore_Movies.Services
+{
+    public class MovieService
+    {
+        private readonly IMongoCollection<Movie> movies;
+
+        public MovieService(IConfiguration config)
+        {
+            MongoClient client = new MongoClient(config.GetConnectionString("MoviesDB"));
+            IMongoDatabase database = client.GetDatabase("MoviesDB");
+            movies = database.GetCollection<Movie>("Movies");
+        }
+
+        public List<Movie> Get()
+        {
+            return movies.Find(movie => true).ToList();
+        }
+
+        public Movie Get(string id)
+        {
+            return movies.Find(movie => movie.Id == id).FirstOrDefault();
+        }
+
+        public Movie Create(Movie movie)
+        {
+            movies.InsertOne(movie);
+            return movie;
+        }
+
+        public void Update(string id, Movie movieIn)
+        {
+            movies.ReplaceOne(movie => movie.Id == id, movieIn);
+        }
+
+        //public void Remove(Movie movieIn)
+        //{
+        //    movies.DeleteOne(movie => movie.Id == movieIn.Id);
+        //}
+
+        public void Remove(string id)
+        {
+            movies.DeleteOne(movie => movie.Id == id);
+        }
+    }
+}
--- solAsp.NetCore_Movies/Asp.NetCore_Movies/Startup.cs
+++ solAsp.NetCore_Movies/Asp.NetCore_Movies/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Asp.NetCore_Movies.Services;
 
 namespace Asp.NetCore_Movies
 {
@@ -24,6 +25,7 @@ namespace Asp.NetCore_Movies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<MovieService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
